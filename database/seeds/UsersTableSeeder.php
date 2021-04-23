b71c2e3a7411f81f3627d113fd47734e23e2af37
mormul_as <mormul_as@groupbwt.com>
2021-04-23 15:25:12 +0300

    feat: added seed for  users

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+/**
+ * UsersTableSeeder
+ */
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'User1',
+            'email' => 'user1@email.com',
+            'password' => Hash::make('password'),
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'user2',
+            'email' => 'user2@email.com',
+            'password' => Hash::make('password'),
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'user3',
+            'email' => 'user3@email.com',
+            'password' => Hash::make('password'),
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'user4',
+            'email' => 'user4@email.com',
+            'password' => Hash::make('password'),
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'user5',
+            'email' => 'user5@email.com',
+            'password' => Hash::make('password'),
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+    }
+}
