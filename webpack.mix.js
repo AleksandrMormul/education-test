7224c1eecf80b8a7ec241d98715f9d04a6104d5d
mormul_as <mormul_as@groupbwt.com>
2021-04-26 15:25:40 +0300

    feat: added blade hot reload

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 const mix = require('laravel-mix');
-
+require('laravel-mix-blade-reload');
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -10,6 +10,7 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
+ mix.js('resources/js/app.js', 'public/js')
+ .bladeReload();
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
