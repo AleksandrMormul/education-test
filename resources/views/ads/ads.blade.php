586322e305a964d7f7880e8d761d445c3469e6e4
mormul_as <mormul_as@groupbwt.com>
2021-04-27 10:41:29 +0300

    fix: removed h5

--- resources/views/ads/ads.blade.php
+++ resources/views/ads/ads.blade.php
@@ -9,7 +9,7 @@
                         <img src="{{ $ad->image ?? 'https://img.lovepik.com/element/40030/1478.png_860.png' }}"
                             class="card-img-top" alt="...">
                         <div class="card-body">
-                            <h5 class="card-title">{{ $ad->title }}</h5>
+                            <p class="card-title">{{ $ad->title }}</p>
                             <p class="card-text">{{ $ad->description }}</p>
                             <p class="card-text">{{ date('d-m-Y', strtotime($ad->created_at)) }}</p>
                             <a href="#" class="btn btn-primary">Go somewhere</a>
