55e3d0cdb1e2317fbfaea789b4a61f7289f65146
mormul_as <mormul_as@groupbwt.com>
2021-04-27 10:04:04 +0300

    fix: changed controller to resource controller

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -4,21 +4,104 @@ namespace App\Http\Controllers;
 
 use App\Services\AdService;
 
-/**
- * AdController
- */
+use Illuminate\Http\Request;
+
 class AdController extends Controller
 {
+
+    private $_adService;
+
     /**
-     * Get all ads
+     * __construct
      *
+     * @param  mixed $service
      * @return void
      */
-    public function getAllAds()
+    public function __construct(AdService $service)
     {
-        $adService = resolve(AdService::class);
-        return view('layouts/ads', [
-            'ads' => $adService->getAds()
-        ]);
+        $this->_adService = $service;
+    }
+
+    /*public function __invoke()
+    {
+
+    }*/
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view(
+            'layouts/ads', [
+            'ads' => $this->_adService->getAds()]
+        );
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
