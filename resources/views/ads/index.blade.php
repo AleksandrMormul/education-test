640fb32b0a15c414e1845a67b359f02ea7dcd5b5
mormul_as <mormul_as@groupbwt.com>
2021-04-29 10:10:09 +0300

    feat: basic logic for render page for ad

--- resources/views/ads/index.blade.php
+++ resources/views/ads/index.blade.php
@@ -6,13 +6,13 @@
             @foreach ($ads as $ad)
                 <div class="col-4 content">
                     <div class="card">
-                        <img src="{{ $ad->image_src ?? asset('/images/temp.png') }}"
+                        <img src="{{ $ad->image_src ?? asset('images/temp.png') }}"
                             class="card-img-top" alt="ad image">
                         <div class="card-body">
                             <h3 class="card-title">{{ $ad->title }}</h3>
                             <p class="card-text">{{ $ad->description }}</p>
                             <p class="card-text">{{ optional($ad->created_at)->toDateString() }}</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <a href="{{ url("/ads/$ad->id") }}" class="btn btn-primary">Show details</a>
                         </div>
                     </div>
                 </div>
---
+++ resources/views/ads/show.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+    <button type="button" class="btn btn btn-link">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"></path>
+        </svg>
+        <a href="{{ url("/ads") }}">Back</a>
+    </button>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-4 content">
+                <div class="card">
+                    <img src="{{ $ad->image_src ?? asset('images/temp.png') }}"
+                         class="card-img-top" alt="ad image">
+                    <div class="card-body">
+                        <h3 class="card-title">{{ $ad->title }}</h3>
+                        <p class="card-text">{{ $ad->description }}</p>
+                        <p class="card-text">{{ optional($ad->created_at)->toDateString() }}</p>
+                        <span class="card-text">Email:
+                            <p class="contact-text">{{ $user->email }}</p>
+                        </span>
+                        <span class="card-text">Phone number:
+                            <p class="contact-text">{{ $user->phone_number ?? 'The user did not set phone number' }}</p>
+                        </span>
+                        <span class="card-text">Country:
+                            <p class="contact-text">{{ $ad->country ?? 'The user did not set country' }}</p>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
