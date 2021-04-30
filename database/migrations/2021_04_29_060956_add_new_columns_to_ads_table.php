54b5bb8cd65e34d684dbb035e49484cbad889e18
mormul_as <mormul_as@groupbwt.com>
2021-04-30 17:38:26 +0300

    fix: added optional arg to latitude

--- database/migrations/2021_04_29_060956_add_new_columns_to_ads_table.php
+++ database/migrations/2021_04_29_060956_add_new_columns_to_ads_table.php
@@ -19,7 +19,7 @@ class AddNewColumnsToAdsTable extends Migration
         Schema::table('ads', function (Blueprint $table) {
             $table->string('country_code', 2)->index()->after('description');
             $table->string('phone_number', 12)->index()->after('description');
-            $table->decimal('latitude')->nullable()->after('phone_number');
+            $table->decimal('latitude', 10, 8)->nullable()->after('phone_number');
             $table->decimal('longitude')->nullable()->after('latitude');
         });
     }
