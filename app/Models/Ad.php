57b6a18805532e7972d451dd39791fb6cf7f3935
mormul_as <mormul_as@groupbwt.com>
2021-05-12 14:03:42 +0300

    feat: changed type end_date in Ads table

--- app/Models/Ad.php
+++ app/Models/Ad.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Lang;
  * @property mixed|null $longitude
  * @property string $country_code
  * @property string|null $img_src
- * @property string $end_date
+ * @property \Illuminate\Support\Carbon $end_date
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @property-read string $full_name_country
@@ -68,6 +68,7 @@ class Ad extends Model
         'user_id' => 'integer',
         'latitude' => 'decimal:8',
         'longitude' => 'decimal:8',
+        'end_date' => 'date'
     ];
 
     /**
