693279f7bacd98c918eeb8bb464a65778792c518
mormul_as <mormul_as@groupbwt.com>
2021-05-17 17:07:18 +0300

    fix: changed FE prepareData function

--- resources/js/plugins/prepareData.js
+++ resources/js/plugins/prepareData.js
@@ -7,20 +7,20 @@ export default function prepareData(markers) {
     const coord = markers[0].getPosition();
     const fullPhoneNumber = document.createElement("input");
     fullPhoneNumber.type = "hidden";
-    fullPhoneNumber.name = "fullPhoneNumber";
+    fullPhoneNumber.name = "phone_number";
     fullPhoneNumber.value = getNumber();
     const inputLat = document.createElement("input");
     inputLat.type = "hidden";
-    inputLat.name = "lat";
+    inputLat.name = "latitude";
     inputLat.value = coord.lat();
     const inputLng = document.createElement("input");
     inputLng.type = "hidden";
-    inputLng.name = "lng";
+    inputLng.name = "longitude";
     inputLng.value = coord.lng();
 
     request.open("POST", "http://localhost/ads");
     formElement.appendChild(inputLat)
     formElement.appendChild(fullPhoneNumber)
     formElement.appendChild(inputLng)
-            request.send(formData);
+    request.send(formData);
 }
