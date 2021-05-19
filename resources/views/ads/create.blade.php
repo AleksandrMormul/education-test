f7f2f83353399b87bb99109a454859e753c7425b
mormul_as <mormul_as@groupbwt.com>
2021-05-19 11:29:56 +0300

    fix: saved old value in inputs

--- resources/views/ads/create.blade.php
+++ resources/views/ads/create.blade.php
@@ -5,6 +5,13 @@
     <script src="https://maps.google.com/maps/api/js?key={{ config('app.google_api_key')}}"></script>
     <script>
         let endDate = new Date('@php echo today()->toDateString() @endphp');
+        $(document).ready(function() {
+            const oldValue = '{{ old('country_code') }}';
+            if(oldValue !== '') {
+                $('#country_code').val(oldValue );
+                $("#country_code").change();
+            }
+        });
     </script>
 @endpush
 
@@ -15,21 +22,22 @@
             @csrf
             <div class="form-group">
                 <label for="title">Title</label>
-                <input type="text" name="title" class="@error('title') is-invalid @enderror form-control" required>
+                <input type="text" name="title" value="{{ old('title') }}" class="@error('title') is-invalid @enderror form-control" required>
                 @error('title')
                     <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
             </div>
             <div class="form-group">
                 <label for="description">Description</label>
-                <textarea class="@error('description') is-invalid @enderror form-control" rows="3" required name="description" minlength="100"></textarea>
+                <textarea class="@error('description') is-invalid @enderror form-control" value="{{ old('description') }}" rows="3" required name="description" minlength="100"></textarea>
                 @error('description')
                     <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
             </div>
             <div class="form-group">
                 <label for="phone">Phone</label>
-                <input type="tel" class="@error('phone_number') is-invalid @enderror  form-control" id="adPhone" name="phone_number" required>
+                <input type="tel" class="@error('phone_number') is-invalid @enderror  form-control" value="{{ old('phone_number') }}"
+                       id="adPhone" name="phone_number" required>
                 @error('phone_number')
                     <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
@@ -38,14 +46,15 @@
             </div>
             <div class="form-group">
                 <label for="email">Email address</label>
-                <input type="email" class="@error('email') is-invalid @enderror form-control" id="adEmail" aria-describedby="emailHelp" required name="email">
+                <input type="email" class="@error('email') is-invalid @enderror form-control" id="adEmail" value="{{ old('email') }}"
+                       aria-describedby="emailHelp" required name="email">
                 @error('email')
                     <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
             </div>
             <div class="form-group">
                 <label for="endDate">End date</label>
-                <input  class="@error('end_date') is-invalid @enderror form-control" id="adEndDate" required name="end_date">
+                <input  class="@error('end_date') is-invalid @enderror form-control" id="adEndDate" value="{{ old('end_date') }}" required name="end_date">
                 @error('end_date')
                     <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
@@ -53,8 +62,9 @@
             <div class="form-group">
                 <label for="country">Country</label>
                 <div>
-                    <select type="text" name="country_code"
-                        class="@error('country') is-invalid @enderror form-control countrySelect" required>
+                    <select type="text" name="country_code" id="country_code"
+                        class="@error('country_code') is-invalid @enderror form-control countrySelect" value="{{ old('country_code') }}"
+                            data-selected-country_code="{{ old('country_code') }}" required>
                         <option></option>
                     @foreach( $countries as $code=>$country)
                         <option value="{{ $code }}">{{ $country }}</option>
