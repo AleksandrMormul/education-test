484112f1140e6e18858bc0e36c7cc1517185457d
mormul_as <mormul_as@groupbwt.com>
2021-05-21 16:39:33 +0300

    fix: added changes in migrations

--- database/migrations/2021_04_27_085136_add_fk_ads_table.php
+++ database/migrations/2021_04_27_085136_add_fk_ads_table.php
@@ -14,7 +14,8 @@ class AddFkAdsTable extends Migration
     public function up()
     {
         Schema::table('ads', function (Blueprint $table) {
-            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')
+                ->onUpdate('cascade');
         });
     }
 
