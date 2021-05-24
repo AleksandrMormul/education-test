3fa584e5a2340b78462e98a048cf6d4cf727b2df
mormul_as <mormul_as@groupbwt.com>
2021-05-24 10:04:39 +0300

    fix: changed migrations name

--- database/migrations/2021_05_21_125115_add_unique_name_to_roles_table.php
+++ database/migrations/2021_05_21_125115_add_unique_name_to_roles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AdUniqueNameToRolesTable extends Migration
+class AddUniqueNameToRolesTable extends Migration
 {
     /**
      * Run the migrations.
