26f4179d0a6491d47c536d2c2f9a04e67e418c6a
mormul_as <mormul_as@groupbwt.com>
2021-05-26 15:36:13 +0300

    fix: changed get current user

--- app/Http/Controllers/FavoriteController.php
+++ app/Http/Controllers/FavoriteController.php
@@ -3,14 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Ad;
-use App\Services\AdService;
 use App\Services\FavoriteService;
-use Illuminate\Contracts\Container\BindingResolutionException;
-use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\View\View;
 
 /**
  * Class FavoriteController
@@ -25,22 +20,7 @@ class FavoriteController extends Controller
      */
     public function toggleFavorite(Request $request, Ad $ad): JsonResponse
     {
-        $user = Auth::user();
-        $data = FavoriteService::toggleFavorite($ad, $user);
+        $data = FavoriteService::toggleFavorite($ad, $request->user());
         return  response()->json($data);
-    }
-
-    /**
-     * @return Application|View
-     * @throws BindingResolutionException
-     */
-    public function index()
-    {
-        $user = Auth::user();
-        $favorites = AdService::getFavoriteAds($user);
-
-        return view('ads.favorite', [
-            'favoritesAds' => $favorites,
-        ]);
     }
 }
--- app/Services/FavoriteService.php
+++ app/Services/FavoriteService.php
@@ -26,14 +26,13 @@ class FavoriteService
             ->where('favoriteable_id', '=', $ad->id)
             ->doesntExist()
         ) {
-            $favorite = new Favorite(['user_id' => Auth::id()]);
+            $favorite = new Favorite(['user_id' => $user->id]);
             $ad->favorites()->save($favorite);
 
             return ['favorite' => 'enabled'];
 
         }
 
-        $user = Auth::user();
         $favorite = UserService::userAdFavorite($user, $ad, Ad::class);
         Favorite::destroy($favorite[0]->id);
 
