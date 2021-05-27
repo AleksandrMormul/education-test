0ddda37f675399df13ef9bbd454ac4e422c5c352
mormul_as <mormul_as@groupbwt.com>
2021-05-27 17:24:19 +0300

    fix: new changes

--- resources/views/ads/show.blade.php
+++ resources/views/ads/show.blade.php
@@ -43,7 +43,7 @@
                         <div class="container-img-ad">
                             <img class="ad-image" src="{{ $ad->image_url }}" alt="ad image">
                             @auth
-                                <i id="heartId-{{$ad->id}}" data-ad-id={{$ad->id}} class="heart  {{ $isFavorite ? 'fas' : 'far'}}  fa-heart"></i>
+                                <i id="heartId-{{$ad->id}}" data-ad-id="{{$ad->id}}" class="heart  {{ $isFavorite ? 'fas' : 'far'}}  fa-heart"></i>
                             @endauth
                         </div>
                         <p>{{ $ad->description }}</p>
