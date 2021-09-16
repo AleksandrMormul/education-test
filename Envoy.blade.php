7852cf9b55d1a48a7fd3e2571d7058212d5ff60a
mormul_as <mormul_as@groupbwt.com>
2021-09-16 10:01:10 +0300

    feat: CI/CD

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -5,22 +5,19 @@
 $releases_dir = $server_dir . '/releases/' . $remove_dir . '';
 $releases_git_dir = $server_dir . '/releases/' . $remove_dir . '/.git';
 $app_dir = $server_dir . '/app';
+
 @endsetup
 
 @story('deploy')
-php
+
 preparation
 run_composer
 update_symlinks
 artisan_command
 frontend_build
 remove_old_releases
-@endstory
 
-@task('php')
-alias php='/usr/bin/php74'
-php -v
-@endtask
+@endstory
 
 @task('preparation')
 
@@ -28,17 +25,20 @@ echo 'Move Folder'
 rm -rf {{$releases_git_dir}}
 cd {{$server_dir}}
 mkdir -p storage
+
 @endtask
 
 @task('run_composer')
-php -v
+
 echo "composer install"
 echo "{{ $releases_dir }}"
 cd {{ $releases_dir }} /
 composer install
+
 @endtask
 
 @task('update_symlinks')
+
 echo "Linking storage directory"
 rm -rf {{$releases_dir}}/storage
 ln -s {{$server_dir}}/storage {{$releases_dir}}/storage
@@ -49,27 +49,33 @@ ln -s {{$server_dir}}/.env {{$releases_dir }}/.env
 echo 'Linking current release'
 rm -rf {{$app_dir}}
 ln -s {{ $releases_dir }} {{ $app_dir }}
+
 @endtask
 
 @task('artisan_command')
 echo 'Artisan Command'
-php {{ $releases_dir }}/artisan view:clear --quiet
-php {{ $releases_dir }}/artisan cache:clear --quiet
-php {{ $releases_dir }}/artisan config:cache --quiet
-php {{ $releases_dir }}/artisan migrate --force
-php {{ $releases_dir }}/artisan storage:link
-php {{ $releases_dir }}/artisan queue:restart --quiet
+php74 {{ $releases_dir }}/artisan view:clear --quiet
+php74 {{ $releases_dir }}/artisan cache:clear --quiet
+php74 {{ $releases_dir }}/artisan config:cache --quiet
+php74 {{ $releases_dir }}/artisan migrate --force
+php74 {{ $releases_dir }}/artisan storage:link
+php74 {{ $releases_dir }}/artisan queue:restart --quiet
 echo "Cache cleared"
+
 @endtask
 
 @task('frontend_build')
+
 echo 'Frontend Build'
 cd {{ $releases_dir }}
 npm install && npm run dev
+
 @endtask
 
 @task('remove_old_releases')
+
 echo 'Remove OLD release'
 cd {{$server_dir}}/releases
 rm -rf `ls -t | tail -n +2`
+
 @endtask
