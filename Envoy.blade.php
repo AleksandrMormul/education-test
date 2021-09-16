46a9c75f0bef4c3d25820c8c71a31350c0df6ee9
mormul_as <mormul_as@groupbwt.com>
2021-09-16 10:04:04 +0300

    feat: CI/CD

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -33,7 +33,11 @@ mkdir -p storage
 echo "composer install"
 echo "{{ $releases_dir }}"
 cd {{ $releases_dir }} /
-composer install
+php74 -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php74 -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php74 composer-setup.php
+php74 -r "unlink('composer-setup.php');"
+php74 ./composer.phar install --prefer-dist -q -o
 
 @endtask
 
